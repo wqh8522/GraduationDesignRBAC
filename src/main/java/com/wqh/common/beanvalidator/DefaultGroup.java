6cc4b8489d49bda8e4bbb886d6b0a2c2c117cc64
wqh3520 <15180181487@163.com>
2018-03-31 09:46:04 +0800

    first commit

--- src/main/java/com/wqh/common/beanvalidator/DefaultGroup.java
+++
@@ -1,9 +0,0 @@
-package com.wqh.common.beanvalidator;
-
-/**
- * 默认Bean验证组
- * 
- */
-public interface DefaultGroup {
-
-}
--- src/main/java/com/wqh/common/beanvalidator/EditGroup.java
+++
@@ -1,9 +0,0 @@
-package com.wqh.common.beanvalidator;
-
-/**
- * 编辑Bena验证组
- * 
- */
-public interface EditGroup {
-
-}
